d40cc3a9c1d7dbc52148ee16bf134304c4c351e9
AlexanderBanar <s595659@mail.ru>
2021-08-01 09:34:19 +0300

    1. Servlet. Web.xml [#6866]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -3,7 +3,6 @@ package ru.job4j.dream.servlet;
 import ru.job4j.dream.Post;
 import ru.job4j.dream.Store;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,10 +12,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostServlet extends HttpServlet {
     private static AtomicInteger postId = new AtomicInteger(4);
-    private static Store store = Store.instOf();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         save(new Post(0, req.getParameter("name"), "empty description", LocalDate.now()));
         resp.sendRedirect(req.getContextPath() + "/posts.jsp");
@@ -24,6 +22,6 @@ public class PostServlet extends HttpServlet {
 
     private void save(Post post) {
         post.setId(postId.incrementAndGet());
-        store.put(post);
+        Store.instOf().put(post);
     }
 }
