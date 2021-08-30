295363ec60aac00aa2246d9dcb6611bb32a92ac7
Alexander.Banar <s6M-HN2-EpL-9CX>
2021-08-30 17:38:34 +0300

    4. Регистрация пользователя. [#283110 #195353]

--- src/main/java/ru/job4j/dream/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/dream/servlet/AuthServlet.java
@@ -4,29 +4,39 @@ import ru.job4j.dream.PsqlStore;
 import ru.job4j.dream.Store;
 import ru.job4j.dream.User;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Store store = PsqlStore.instOf();
+        PrintWriter pw = resp.getWriter();
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        Store store = PsqlStore.instOf();
-        if (store.findByEmail(email).getPassword().equals(password)) {
-            HttpSession sc = req.getSession();
-            User user = new User();
-            user.setEmail(email);
-            user.setPassword(password);
-            sc.setAttribute("user", user);
-            resp.sendRedirect(req.getContextPath() + "/posts.do");
+        pw.println(email);
+        pw.println(password);
+        User userSearchedByEmail = store.findByEmail(email);
+        if (userSearchedByEmail != null) {
+            if (userSearchedByEmail.getPassword().equals(password)) {
+                HttpSession sc = req.getSession();
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(password);
+                sc.setAttribute("user", user);
+                resp.sendRedirect(req.getContextPath() + "/posts.do");
+            } else {
+                req.setAttribute("error", "Не верный email или пароль");
+                req.getRequestDispatcher("login.jsp").forward(req, resp);
+            }
         } else {
-            req.setAttribute("error", "Не верный email или пароль");
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
+            pw.print("User is null");
         }
     }
 }
