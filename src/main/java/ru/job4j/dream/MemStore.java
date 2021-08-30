16e0a1834f19ced541cffd8689961792b47920bf
Alexander.Banar <s6M-HN2-EpL-9CX>
2021-08-30 18:10:52 +0300

    1. Написать тест на PostServlet. [#2518]

--- src/main/java/ru/job4j/dream/MemStore.java
+++ src/main/java/ru/job4j/dream/MemStore.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MemStore {
+public class MemStore implements Store {
     private static final MemStore INST = new MemStore();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
@@ -13,45 +13,61 @@ public class MemStore {
     private static final AtomicInteger CAN_ID = new AtomicInteger(6);
 
     private MemStore() {
-        posts.put(1, new Post(1, "Junior Java Job"));
-        posts.put(2, new Post(2, "Middle Java Job"));
-        posts.put(3, new Post(3, "Senior Java Job"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
+//        posts.put(1, new Post(1, "Junior Java Job"));
+//        posts.put(2, new Post(2, "Middle Java Job"));
+//        posts.put(3, new Post(3, "Senior Java Job"));
+//        candidates.put(1, new Candidate(1, "Junior Java"));
+//        candidates.put(2, new Candidate(2, "Middle Java"));
+//        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static MemStore instOf() {
         return INST;
     }
 
+    @Override
     public Collection<Post> findAllPosts() {
         return posts.values();
     }
 
+    @Override
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
     }
 
-    public void save(Post post) {
+    @Override
+    public void savePost(Post post) {
         if (post.getId() == 0) {
             post.setId(POST_ID.incrementAndGet());
         }
         posts.put(post.getId(), post);
     }
 
-    public void save(Candidate candidate) {
+    @Override
+    public void saveCan(Candidate candidate) {
         if (candidate.getId() == 0) {
             candidate.setId(CAN_ID.incrementAndGet());
         }
         candidates.put(candidate.getId(), candidate);
     }
 
+    @Override
     public Post findPostById(int id) {
         return posts.get(id);
     }
 
+    @Override
     public Candidate findCanById(int id) {
         return candidates.get(id);
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
     }
 }
---
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.dream.servlet;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import ru.job4j.dream.Post;
+import ru.job4j.dream.PsqlStore;
+import ru.job4j.dream.MemStore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PsqlStore.class)
+public class PostServletTest {
+    @Test
+    public void whenCreatePost() throws IOException {
+        MemStore store = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        PowerMockito.when(req.getParameter("id")).thenReturn("0");
+        PowerMockito.when(req.getParameter("name")).thenReturn("n");
+        new PostServlet().doPost(req, resp);
+        Post result = store.findAllPosts().iterator().next();
+        Assert.assertThat(result.getName(), Is.is("n"));
+    }
+}
